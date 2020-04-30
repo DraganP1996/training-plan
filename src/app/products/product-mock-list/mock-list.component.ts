bc9c0f825ed6556b32fb236dca037104a6ac6ff2
DraganP1996 <pdragan1996@gmail.com>
2020-04-30 11:48:05 +0200

    Adding modal dialog on mock/list, the images of the products are available in the modal dialog of products/list, there is no this functionality on products/mock-list

--- src/app/products/product-mock-list/mock-list.component.ts
+++ src/app/products/product-mock-list/mock-list.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { DataService } from '../../core/services/data.service';
 import { Product } from '../../models/product.model';
 
+import { MatDialog } from '@angular/material/dialog';
+import { ProductDialog } from '../product-list/product-list.component';
 @Component({
     selector:'mock-list',
     templateUrl:'./mock-list.component.html',
@@ -9,8 +11,15 @@ import { Product } from '../../models/product.model';
 })
 export class MockListComponent {
     products: Product[];
-    constructor(public dataService: DataService){
+    constructor(public dataService: DataService, private matDialog: MatDialog){
         this.products = new Array();
         this.dataService.getProducts().subscribe(p => this.products.push(p));
     }
+
+    openDialog(product: Product) {
+        const dialogRef = this.matDialog.open(ProductDialog, {
+          width: '650px',
+          data: product,
+        });
+      }    
 }
